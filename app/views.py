fbf037e0937eb85456270c9aef73cc6fb2a7414f
EricSchles <ericschles@gmail.com>
2014-11-27 13:56:36 -0800

    things

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from flask import request, render_template, jsonify
+from app import app
 
 @app.route("/<name>", methods=["GET","POST"])
 @app.route("/")
@@ -7,16 +8,18 @@ def intake(name=''):
         obj = jsonify({"name":"Eric Schles",
                         "job":"fightin' slavery",
                         "email":"eric.schles@syncano.com"})
+        return render_template("index.html",obj=obj.get_data())
     elif name == "Lior":
         obj = jsonify({"name":"Lior Shahverdi",
                         "job":"nope",
                         "email":"like I'd tell you..."})
+        return render_template("index.html",obj=obj.get_data())
     else:
         obj=''
-    return render_template("index.html",obj=obj)
+        return render_template("index.html",obj=obj)
 
 @app.route("/get",methods=["GET","POST"])
-def get_input():
+def get():
     name = request.form.get("name")
     id = request.form.get("id")
     if name == "Eric":
